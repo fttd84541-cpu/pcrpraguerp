c0a57ae6192ef4dc40bb7aa64f86515c0fa735d4
yxsm_rico <fttd84541@gmail.com>
2026-05-28 14:38:22 +0200

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,8 @@
+window.firebaseConfig = {
+  apiKey: "AIzaSyAzjuuppj_zuqA_FRG7WIug68P7SCpbsUI",
+  authDomain: "praguerppcr.firebaseapp.com",
+  projectId: "praguerppcr",
+  storageBucket: "praguerppcr.appspot.com",
+  messagingSenderId: "111189472977",
+  appId: "1:111189472977:web:4262fd2f86d3166d0cf7f0"
+};
